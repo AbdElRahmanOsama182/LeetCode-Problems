6f1465136eab44dc012f6ec2abf0d7a95dd96e8d
AbdElRahman Osama <52374998+AbdElRahmanOsama182@users.noreply.github.com>
2024-03-15 06:47:30 +0200

    Time: 110 ms (81.33%), Space: 100.8 MB (98.88%) - LeetHub

---
+++ 0739-daily-temperatures/0739-daily-temperatures.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    vector<int> dailyTemperatures(vector<int>& temperatures) {
+        vector<int> ans(temperatures.size(),0);
+        stack<int> st;
+        for (int i = temperatures.size()-1; i >= 0; i--) {
+            while (!st.empty() && temperatures[i] >= temperatures[st.top()]) st.pop();
+            if (!st.empty()) ans[i] = st.top()-i;
+            st.push(i);
+        }
+        return ans;        
+    }
+};
