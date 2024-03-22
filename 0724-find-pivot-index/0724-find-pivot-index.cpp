a57148db56659d06f27353e0690e4607ec823640
AbdElRahman Osama <52374998+AbdElRahmanOsama182@users.noreply.github.com>
2024-03-22 18:25:29 +0200

    Time: 8 ms (97.21%), Space: 34.6 MB (23.77%) - LeetHub

---
+++ 0724-find-pivot-index/0724-find-pivot-index.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int pivotIndex(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> pref(n+1, 0);
+        for (int i = 1; i <= n; i++){
+            pref[i] = pref[i-1] + nums[i-1];
+        }
+        for (int i = 1; i <= n; i++){
+            if (pref[i-1] == pref[n] - pref[i]){
+                return i-1;
+            }
+        }
+        return -1;
+    }
+};
