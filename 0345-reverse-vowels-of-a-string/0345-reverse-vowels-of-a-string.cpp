3343ffa36d8b9fa6bcdebae8282702b99a8e6636
AbdElRahman Osama <52374998+AbdElRahmanOsama182@users.noreply.github.com>
2024-03-25 00:51:51 +0200

    Time: 8 ms (44.4%), Space: 9.2 MB (59.83%) - LeetHub

--- 0345-reverse-vowels-of-a-string/0345-reverse-vowels-of-a-string.cpp
+++ 0345-reverse-vowels-of-a-string/0345-reverse-vowels-of-a-string.cpp
@@ -1,3 +1,5 @@
+#define fastIO        ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+
 class Solution {
 public:
     bool isVowel(char c){
@@ -5,6 +7,7 @@ public:
     }
 
     string reverseVowels(string s) {
+        fastIO
         int n = s.size();
         int i = 0, j = n-1;
         while (i < j){
