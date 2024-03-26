aed67824a4222459458d0f6df00bbcfbbcbd23c8
AbdElRahman Osama <bodyusama207@gmail.com>
2024-03-26 21:39:27 +0200

    Time: 3 ms (47.38%), Space: 7.4 MB (69.66%) - LeetHub

--- 0392-is-subsequence/0392-is-subsequence.cpp
+++ 0392-is-subsequence/0392-is-subsequence.cpp
@@ -2,7 +2,6 @@ class Solution {
 public:
     bool isSubsequence(string s, string t) {
         int n = s.size(), m = t.size();
-        if (n > m) return false;
         int i = 0, j = 0;
         while (i < n && j < m){
             if (s[i] == t[j++])
