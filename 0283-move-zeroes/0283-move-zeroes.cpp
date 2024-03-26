bf73df9c94ebe8e23f7864d74ba3d0c7282f7115
AbdElRahman Osama <52374998+AbdElRahmanOsama182@users.noreply.github.com>
2024-03-26 02:00:58 +0200

    Time: 21 ms (21.11%), Space: 21.5 MB (95.92%) - LeetHub

---
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -0,0 +1,9 @@
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+        int n = nums.size(), last = 0;
+        for (int i = 0; i < n; i++)
+            if (nums[i] != 0)
+                swap(nums[i], nums[last++]);
+    }
+};
