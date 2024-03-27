be6646b70c5bc49e4aa1c7c3bdeffef3acbcdd05
AbdElRahman Osama <bodyusama207@gmail.com>
2024-03-27 04:21:14 +0200

    Time: 83 ms (72.18%), Space: 28.3 MB (22.85%) - LeetHub

---
+++ 2390-removing-stars-from-a-string/2390-removing-stars-from-a-string.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    string removeStars(string s) {
+        stack<char> st;
+        for (char c : s) 
+            if (c == '*') st.pop();
+            else st.push(c);
+        string res;
+        while (!st.empty()) {
+            res += st.top();
+            st.pop();
+        }
+        reverse(res.begin(), res.end());
+        return res;
+    }
+};
