e110f7de89d78fbcadc8dfbc00bd20f0bc1a554c
AbdElRahman Osama <bodyusama207@gmail.com>
2024-03-28 05:06:08 +0200

    Time: 190 ms (92.68%), Space: 148.5 MB (57.21%) - LeetHub

--- 2958-length-of-longest-subarray-with-at-most-k-frequency/2958-length-of-longest-subarray-with-at-most-k-frequency.cpp
+++ 2958-length-of-longest-subarray-with-at-most-k-frequency/2958-length-of-longest-subarray-with-at-most-k-frequency.cpp
@@ -4,13 +4,10 @@ public:
         int n = nums.size(), st = 0, ans = 0;
         unordered_map<int, int> mp;
         for (int i = 0; i < n; i++){
-            mp[nums[i]]++;
-            if (mp[nums[i]] > k) {
+            if (++mp[nums[i]] > k) {
                 ans = max(ans, i - st);
-                while (mp[nums[i]] > k){
-                    mp[nums[st]]--;
-                    st++;
-                }
+                while (mp[nums[i]] > k)
+                    mp[nums[st++]]--;
             }
         }
         ans = max(ans, n - st);
