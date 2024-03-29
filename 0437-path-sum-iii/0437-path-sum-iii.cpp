35427b9c7a20c350a6d648df25b5d33a4959f911
AbdElRahman Osama <bodyusama207@gmail.com>
2024-03-29 07:18:40 +0200

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0437-path-sum-iii/0437-path-sum-iii.cpp
@@ -0,0 +1,24 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+
+    int dfs(TreeNode* root, int targetSum) {
+        if (!root) return 0;
+        return (root->val == targetSum) + dfs(root->left, targetSum - root->val) + dfs(root->right, targetSum - root->val);
+    }
+
+    int pathSum(TreeNode* root, int targetSum) {
+        if (!root) return 0;
+        return dfs(root, targetSum) + pathSum(root->left, targetSum) + pathSum(root->right, targetSum);
+    }
+};
