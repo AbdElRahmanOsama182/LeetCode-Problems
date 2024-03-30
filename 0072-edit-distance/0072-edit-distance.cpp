62df9285cc8f85f0fe8dd9062408b5dd92f383c7
AbdElRahman Osama <bodyusama207@gmail.com>
2024-03-30 22:42:29 +0200

    Time: 7 ms (64.15%), Space: 12.7 MB (18.66%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.cpp
+++ 0072-edit-distance/0072-edit-distance.cpp
@@ -1,16 +1,22 @@
 class Solution {
 public:
-
-    int solve(vector<vector<int>> &dp, string &s1, string &s2, int i, int j) {
-        if (i == s1.size()) return s2.size() - j;
-        if (j == s2.size()) return s1.size() - i;
-        if (dp[i][j] != -1) return dp[i][j];
-        if (s1[i] == s2[j]) return dp[i][j] = solve(dp, s1, s2, i+1, j+1);
-        return dp[i][j] = 1 + min({solve(dp, s1, s2, i+1, j), solve(dp, s1, s2, i, j+1), solve(dp, s1, s2, i+1, j+1)});
-    }
-
+    vector<vector<int>> dp;
+    int n, m;
     int minDistance(string word1, string word2) {
-        vector<vector<int>> dp(word1.size()+1, vector<int>(word2.size()+1, -1));
-        return solve(dp, word1, word2, 0, 0);
+        n = word1.size();
+        m = word2.size();
+        dp = vector<vector<int>>(n+1, vector<int>(m+1, -1));
+        return solve(word1, word2, 0, 0);        
+    }
+    int solve(string &word1, string &word2, int i, int j){
+        if (i == n) return m-j;
+        if (j == m) return n-i;
+        if (dp[i][j] != -1) return dp[i][j];
+        if (word1[i] == word2[j]) 
+            return dp[i][j] = solve(word1, word2, i+1, j+1); // no operation needed
+        int insert = 1 + solve(word1, word2, i, j+1);
+        int remove = 1 + solve(word1, word2, i+1, j);
+        int replace = 1 + solve(word1, word2, i+1, j+1);
+        return dp[i][j] = min({insert, remove, replace});
     }
 };
