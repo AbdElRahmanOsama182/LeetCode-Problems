374fb96db632c053610bede964fa3dd85d1f0dd1
AbdElRahman Osama <bodyusama207@gmail.com>
2024-03-31 23:00:36 +0200

    Time: 98 ms (50.25%), Space: 64.4 MB (10.37%) - LeetHub

---
+++ 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int findKthLargest(vector<int>& nums, int k) {
+        priority_queue<int> pq;
+        for(auto i:nums) pq.push(i);
+        while(k>1){
+            pq.pop();
+            k--;
+        }
+        return pq.top();
+    }
+};
