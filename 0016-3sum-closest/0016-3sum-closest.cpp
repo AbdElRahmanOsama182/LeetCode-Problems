a3003d7e9258e955e2d5393eeff3319294a7d971
AbdElRahman Osama <bodyusama207@gmail.com>
2024-04-06 00:21:50 +0200

    Time: 12 ms (85.9%), Space: 12.9 MB (19.3%) - LeetHub

--- 0016-3sum-closest/0016-3sum-closest.cpp
+++ 0016-3sum-closest/0016-3sum-closest.cpp
@@ -3,7 +3,7 @@ public:
     int threeSumClosest(vector<int>& nums, int target) {
         int n = nums.size();
         sort(nums.begin(), nums.end());
-        int ans = INT_MAX;
+        int ans = 5e4;
         for(int i=0; i<n-2; i++){
             if (i > 0 && nums[i] == nums[i-1]) continue;
             int l = i+1, r = n-1;
