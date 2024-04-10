bf68911c0ed056e261000d82fd467d4206e3d535
AbdElRahman Osama <bodyusama207@gmail.com>
2024-04-11 00:51:53 +0200

    Time: 0 ms (100%), Space: 10.9 MB (22.82%) - LeetHub

--- 0274-h-index/0274-h-index.cpp
+++ 0274-h-index/0274-h-index.cpp
@@ -3,10 +3,15 @@ public:
     int hIndex(vector<int>& citations) {
         int n = citations.size(), h, ans = 0;
         sort(citations.begin(), citations.end());
-        for (int i = 0; i < n; i++) {
-            h = min(citations[i], n - i);
-            ans = max(ans, h);
+        int l = 0, r = n-1, mid;
+        while (l <= r){
+            mid = l + (r-l)/2;
+            h = n-mid;
+            if (citations[mid] < n-mid)
+                l = mid+1;
+            else
+                r = mid-1;
         }
-        return ans;
+        return n-l;
     }
 };
