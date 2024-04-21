12a7a856ee852b2cc72b513fe450f47c3497ebee
AbdElRahman Osama <bodyusama207@gmail.com>
2024-04-21 22:51:15 +0200

    Time: 337 ms (83.81%), Space: 140.3 MB (83.19%) - LeetHub

--- 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.cpp
+++ 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.cpp
@@ -1,22 +1,23 @@
 class Solution {
 public:
     bool validPath(int n, vector<vector<int>>& edges, int source, int destination) {
-        vector<vector<int>> adj(n);
-        vector<bool> vis(n, false);
-        for (auto e:edges) {
-            adj[e[0]].push_back(e[1]);
-            adj[e[1]].push_back(e[0]);
-        }
-        dfs(source,adj,vis);
-        return vis[destination];
+        vector<int> p(n), sz(n,1);
+        for (int i = 0; i < n; i++)
+            p[i] = i;
+        for (auto e:edges)
+            join(e[0],e[1],p,sz);
+        return find(source,p) == find(destination,p);
     }
 
-    void dfs(int v, vector<vector<int>>& adj, vector<bool>& vis) {
-        if (vis[v]) return;
-        vis[v] = true;
-        for (auto u:adj[v]) {
-            if(!vis[u])
-                dfs(u,adj,vis);
-        }
+    int find(int x, vector<int>& p) {
+        return p[x] = (p[x]==x)?x:find(p[x],p);
+    }
+    void join(int u, int v, vector<int>& p, vector<int>& sz) {
+        u = find(u, p);
+        v = find(v, p);
+        if (u == v) return;
+        if (sz[u] < sz[v]) swap(u,v);
+        p[v] = u;
+        sz[u]+=sz[v];
     }
 };
