3a963bdcb8f35b870c2af8a7baa4a03b250ea973
AbdElRahman Osama <bodyusama207@gmail.com>
2024-04-22 01:34:09 +0200

    Time: 0 ms (100%), Space: 8.9 MB (59.06%) - LeetHub

--- 0046-permutations/0046-permutations.cpp
+++ 0046-permutations/0046-permutations.cpp
@@ -1,11 +1,21 @@
 class Solution {
 public:
+    vector<vector<int>> ans;
     vector<vector<int>> permute(vector<int>& nums) {
-        vector<vector<int>> p;
-        sort(nums.begin(),nums.end());
-        do{
-            p.push_back(nums);
-        } while (next_permutation(nums.begin(),nums.end()));
-        return p;
+        perm(nums,0,nums.size()-1);
+        return ans;
+    }
+    void perm(vector<int>& nums, int l, int r) {
+        if (l==r) {
+            ans.push_back(nums);
+        }
+        else {
+            for (int i = l; i <= r; i++)
+            {
+                swap(nums[l],nums[i]);
+                perm(nums,l+1,r);
+                swap(nums[l],nums[i]);
+            }            
+        }
     }
 };
