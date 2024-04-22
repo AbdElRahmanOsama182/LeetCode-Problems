ae32c30420cf4405bdda2a819800007e0e320289
AbdElRahman Osama <bodyusama207@gmail.com>
2024-04-22 13:55:02 +0200

    Time: 3 ms (34.31%), Space: 8.8 MB (99.55%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -2,14 +2,11 @@ class Solution {
 public:
     int rob(vector<int>& nums) {
         int n = nums.size();
-        if (n == 1) return nums[0];
-        if (n == 2) return max(nums[0], nums[1]);
-        vector<int> dp(n);
-        dp[0] = nums[0];
-        dp[1] = nums[1];
-        dp[2] = nums[2] + nums[0];
+        if (n == 1) return nums[0]; 
+        if (n == 2) return max(nums[0],nums[1]); 
+        nums[2] += nums[0];
         for (int i = 3; i < n; i++) 
-            dp[i] = nums[i] + max(dp[i-2], dp[i-3]);
-        return max(dp[n-1], dp[n-2]);
+            nums[i] += max(nums[i-2], nums[i-3]);
+        return max(nums[n-1],nums[n-2]);
     }
 };
