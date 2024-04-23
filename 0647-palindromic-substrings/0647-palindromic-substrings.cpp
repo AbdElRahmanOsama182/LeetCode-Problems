f67edddb5bce1be5d3ce4f1c32e4ada04a653fe4
AbdElRahman Osama <bodyusama207@gmail.com>
2024-04-23 16:19:03 +0200

    Time: 6 ms (77.75%), Space: 7.4 MB (84.76%) - LeetHub

--- 0647-palindromic-substrings/0647-palindromic-substrings.cpp
+++ 0647-palindromic-substrings/0647-palindromic-substrings.cpp
@@ -2,18 +2,18 @@ class Solution {
 public:
     int countSubstrings(string s) {
         int n = s.size(), ans = 0;
-        for(int i=0; i<n; i++){
+        for (int i = 0; i < n; i++) {
             int l = i, r = i;
-            while(l>=0 && r<n && s[l] == s[r]){
+            while (l > -1 && r < n && s[l] == s[r]) {
                 ans++;
-                l--;
                 r++;
+                l--;
             }
             l = i, r = i+1;
-            while(l>=0 && r<n && s[l] == s[r]){
+            while (l > -1 && r < n && s[l] == s[r]) {
                 ans++;
-                l--;
                 r++;
+                l--;
             }
         }
         return ans;
