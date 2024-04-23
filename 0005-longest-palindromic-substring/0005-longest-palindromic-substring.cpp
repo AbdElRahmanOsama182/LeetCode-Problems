f427b1f05431216c806052d45601d76fcfc7c151
AbdElRahman Osama <bodyusama207@gmail.com>
2024-04-23 17:00:43 +0200

    Time: 3 ms (99.45%), Space: 13 MB (57.67%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
@@ -1,40 +1,30 @@
 class Solution {
 public:
-
-    int solve(vector<vector<int>>& dp, string &s, int i, int j) {
-        if (i>j) return 0;
-        if (i==j) {
-            dp[i][j] = 1;
-            return 1;
-        }
-        if (dp[i][j]!=-1) return dp[i][j];
-        dp[i][j] = 0;
-        if (j - i == 1) {
-            if (s[i] == s[j]) {
-                dp[i][j] = 1;
-            }
-            return dp[i][j];
-        }
-        if (s[i] == s[j] && solve(dp, s, i + 1, j - 1) == 1) {
-            dp[i][j] = 1;
-        }
-        return dp[i][j];
-    }
-
     string longestPalindrome(string s) {
-        int n = s.size();
-        vector<vector<int>> dp(n, vector<int>(n, -1));
-        int maxLen = 0;
-        int start = 0;
+        int n = s.size(), l, r , maxL = -1;
+        string ans ="";
         for (int i = 0; i < n; i++) {
-            for (int j = i; j < n; j++) {
-                solve(dp, s, i, j);
-                if (dp[i][j] == 1 && j - i + 1 > maxLen) {
-                    maxLen = j - i + 1;
-                    start = i;
-                }
+            l = i, r = i;
+            while (l > -1 && r < n && s[l] == s[r]) {
+                l--;
+                r++;
+            }
+            l++,r--;
+            if (r-l+1 > maxL) {
+                maxL = r-l+1;
+                ans = s.substr(l,maxL);
+            }
+            l = i, r = i+1;
+            while (l > -1 && r < n && s[l] == s[r]) {
+                l--;
+                r++;
+            }
+            l++,r--;
+            if (r-l+1 > maxL) {
+                maxL = r-l+1;
+                ans = s.substr(l,maxL);
             }
         }
-        return s.substr(start, maxLen);
+        return ans;
     }
 };
