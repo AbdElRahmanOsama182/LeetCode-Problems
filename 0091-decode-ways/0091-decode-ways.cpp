ea89b3f88647bf203705c922124d2b6ffe084224
AbdElRahman Osama <bodyusama207@gmail.com>
2024-04-23 16:47:43 +0200

    Time: 4 ms (31.71%), Space: 7.5 MB (91.06%) - LeetHub

---
+++ 0091-decode-ways/0091-decode-ways.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int numDecodings(string s) {
+        if (s[0] == '0') return 0;
+        int n = s.size();
+        if ( n == 1) return 1;
+        vector<int> dp(n+2, 0);
+        dp[n-1] = (s[n-1] == '0')?0:1;
+        dp[n] = 1;
+        cout<<s[n-1]<<"->"<<dp[n-1]<<" ";
+        for (int i = n-2; i >= 0; i--) {
+            if (s[i] =='0') continue;
+            dp[i] += dp[i+1];
+            int x = (s[i]-'0')*10+(s[i+1]-'0');
+            cout<<x<<endl;
+            if (x < 27) dp[i] += dp[i+2];
+            cout<<s[i]<<"->"<<dp[i]<<" ";
+        }
+        return dp[0];
+    }
+};
