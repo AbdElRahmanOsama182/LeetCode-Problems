03a7cca9b2796ff29d641298d9039b7b66c1fb3f
AbdElRahman Osama <bodyusama207@gmail.com>
2024-04-25 14:22:12 +0200

    Time: 0 ms (100%), Space: 11.3 MB (49.47%) - LeetHub

--- 0162-find-peak-element/0162-find-peak-element.cpp
+++ 0162-find-peak-element/0162-find-peak-element.cpp
@@ -1,14 +1,16 @@
 class Solution {
 public:
     int findPeakElement(vector<int>& nums) {
-        int l = 0, r = nums.size()-1, mid;
-        while (l < r){
-            mid = l + (r-l)/2;
-            if (nums[mid] < nums[mid+1])
-                l = mid+1;
-            else
-                r = mid;
+        int st = 0, mid, ed = nums.size()-1;
+        while (st < ed) {
+            mid = st + (ed - st) / 2;
+            if (nums[mid] < nums[mid+1]) {
+                st = mid + 1;
+            }
+            else {
+                ed = mid;
+            }
         }
-        return l;
+        return st;
     }
 };
