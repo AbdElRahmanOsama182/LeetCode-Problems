69b3cd1ddbf361b7773c664439f31ffda2f260d8
AbdElRahman Osama <bodyusama207@gmail.com>
2024-04-25 13:19:10 +0200

    Time: 49 ms (72.27%), Space: 11.3 MB (99.16%) - LeetHub

--- 2370-longest-ideal-subsequence/2370-longest-ideal-subsequence.cpp
+++ 2370-longest-ideal-subsequence/2370-longest-ideal-subsequence.cpp
@@ -1,28 +1,20 @@
 class Solution {
 public:
-    int solve(string& s, int k, int st, int ed) {
-        int ans = 0;
-        if (st == s.size()) return 0;
-        if (ed == s.size()) {
-            ans = solve(s,k,st+1,st+2);
-        }
-        else {
-        int diff = abs(s[st]-s[ed]);
-        cout<<"diff "<< s[st] << " " << s[ed] << " "<<diff<<endl;
-        if (diff <= k){
-            ans = max(solve(s,k,ed,ed+1)+1,solve(s,k,st+1,st+2));
-        }
-        else
-            ans = max(solve(s,k,st,ed+1),solve(s,k,st+1,st+2));
-        }
-    
-        cout <<st<<" "<<ed <<" => "<<ans <<endl;
-        return ans;
-    }
     int longestIdealString(string s, int k) {
         int n = s.size();
-        if (n == 1) return 1;
-        vector<int> dp(n+1,0);
-        return solve(s,k,0, 1)+1;
+        vector<int> dp(26, 0);
+        int ans = 0;
+        for (int i = 0; i < n; i++) {
+            int curr = s[i] - 'a';
+            int longest = 0;
+            for (int j = 0; j < 26; j++) {
+                if (abs(curr - j) <= k) {
+                    longest = max(longest, dp[j]);
+                }
+            }
+            dp[curr] = max(dp[curr], longest + 1);
+            ans = max(ans, dp[curr]);
+        }
+        return ans;
     }
 };
