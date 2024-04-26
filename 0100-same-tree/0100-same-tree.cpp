40dfd686d5db3897306b28890737b60a3af36346
AbdElRahman Osama <bodyusama207@gmail.com>
2024-04-26 23:52:38 +0300

    Time: 0 ms (100%), Space: 11.9 MB (20.72%) - LeetHub

--- 0100-same-tree/0100-same-tree.cpp
+++ 0100-same-tree/0100-same-tree.cpp
@@ -12,8 +12,8 @@
 class Solution {
 public:
     bool isSameTree(TreeNode* p, TreeNode* q) {
-        if (p == NULL && q == NULL) return true;
-        if (p == NULL || q == NULL) return false;
+        if (!p && !q) return true;
+        if (!p || !q) return false;
         if (p->val != q->val) return false;
         return isSameTree(p->left, q->left) && isSameTree(p->right, q->right);
     }
