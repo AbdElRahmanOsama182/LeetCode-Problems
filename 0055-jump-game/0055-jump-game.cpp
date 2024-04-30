49dbfeeec9fd4950d66e929288926048ecf645b7
AbdElRahman Osama <bodyusama207@gmail.com>
2024-04-30 20:02:25 +0300

    Time: 42 ms (71.38%), Space: 50.9 MB (22.9%) - LeetHub

--- 0055-jump-game/0055-jump-game.cpp
+++ 0055-jump-game/0055-jump-game.cpp
@@ -1,16 +1,11 @@
 class Solution {
 public:
     bool canJump(vector<int>& nums) {
-        int n = nums.size();
-        if (n == 1) return true;
-        if (nums[0] == 0) return false;
-        int lastZero = -1;
-        for (int i = n - 2; i >= 0; i--){
-            if (nums[i] == 0)
-                lastZero = max(lastZero, i);
-            else if (lastZero != -1 && nums[i] + i > lastZero)
-                lastZero = -1;
+        int reach = 0;
+        for (int i = 0; i < nums.size(); i++) {
+            if (reach < i) return false;
+            reach = max(reach, i + nums[i]);
         }
-        return lastZero == -1;
+        return true;
     }
 };
