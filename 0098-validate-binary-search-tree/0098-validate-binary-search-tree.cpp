f40439a894aedc14854fdf09e6dfa3d33a55d21d
AbdElRahman Osama <bodyusama207@gmail.com>
2024-05-01 00:58:37 +0300

    Time: 7 ms (75.78%), Space: 20.3 MB (28.4%) - LeetHub

--- 0098-validate-binary-search-tree/0098-validate-binary-search-tree.cpp
+++ 0098-validate-binary-search-tree/0098-validate-binary-search-tree.cpp
@@ -6,8 +6,7 @@
  *     TreeNode *right;
  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
- * right(right) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
 class Solution {
@@ -16,13 +15,11 @@ public:
         return isValidBST(root, nullptr, nullptr);
     }
 
-    bool isValidBST(TreeNode* root, TreeNode* min, TreeNode* max) {
-        if (!root)
-            return true;
-        if ((min && root->val <= min->val) || (max && root->val >= max->val)) {
-            return false;
-        }
-        return isValidBST(root->left, min, root) &&
-               isValidBST(root->right, root, max);
+    bool isValidBST(TreeNode* root, TreeNode* Min, TreeNode* Max) {
+        if (!root) return true;
+        if (Min && root->val <= Min->val) return false;
+        if (Max && root->val >= Max->val) return false;
+        return isValidBST(root->left, Min, root) && isValidBST(root->right, root, Max);
+
     }
 };
