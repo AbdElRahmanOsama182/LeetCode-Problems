641934c7cb86473cae2e3d4ac16de808cb1d9920
AbdElRahman Osama <bodyusama207@gmail.com>
2024-04-30 22:38:28 +0300

    Time: 20 ms (27.1%), Space: 21.7 MB (37.66%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.cpp
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -1,9 +1,9 @@
 class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
-        int n = nums.size(), last = 0;
-        for (int i = 0; i < n; i++)
-            if (nums[i] != 0)
-                swap(nums[i], nums[last++]);
+        int last = 0;
+        for (int i = 0; i < nums.size(); i++) {
+            if (nums[i] != 0) swap(nums[i], nums[last++]);
+        }
     }
 };
