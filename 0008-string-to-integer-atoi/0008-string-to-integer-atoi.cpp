e4a6ec6d0aae9e79ee69a7cbf2cf5753f9bdf727
AbdElRahman Osama <bodyusama207@gmail.com>
2024-05-01 02:51:34 +0300

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0008-string-to-integer-atoi/0008-string-to-integer-atoi.cpp
+++ 0008-string-to-integer-atoi/0008-string-to-integer-atoi.cpp
@@ -2,22 +2,19 @@ class Solution {
 public:
     int myAtoi(string s) {
         bool neg = false, skipSpaces = true;
-        long long ans = 0;
+        long ans = 0;
         for (char c : s) {
             if (c == ' ' && skipSpaces) continue;
-            else if (c == '-' && skipSpaces) {
-                skipSpaces = false;
+            if (c =='-' && skipSpaces) {
                 neg = true;
+                skipSpaces = false;
             }
-            else if (c == '+' && skipSpaces) {
+            else if (c =='+' && skipSpaces) {
                 skipSpaces = false;
             }
             else if (c >= '0' && c <= '9') {
-                skipSpaces = false;
                 ans = ans * 10 + (c - '0');
-                if (ans > INT_MAX) {
-                    return neg? INT_MIN : INT_MAX;
-                }
+                if (ans > INT_MAX) return neg? INT_MIN : INT_MAX;
             }
             else break;
         }
