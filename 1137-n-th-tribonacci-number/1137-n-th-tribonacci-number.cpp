e281ca75d32ab6bcf4e4869ef933919638415252
AbdElRahman Osama <bodyusama207@gmail.com>
2024-04-30 23:55:33 +0300

    Time: 3 ms (23.72%), Space: 7 MB (80.41%) - LeetHub

--- 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.cpp
+++ 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.cpp
@@ -2,13 +2,13 @@ class Solution {
 public:  
     int tribonacci(int n) {
         if (n == 0) return 0;
-        int prev = 1, prev1 = 1, prev2 = 0, curr = 1;
+        int prev0 = 0, prev1 = 1, prev2 = 1;
         for (int i = 3; i <= n; i++) {
-            curr = prev+prev1+prev2;
-            prev2 = prev1;
-            prev1 = prev;
-            prev = curr;
+            int temp = prev2;
+            prev2 += prev1 + prev0;
+            prev0 = prev1;
+            prev1= temp;
         }
-        return curr;
+        return prev2;
     }
 };
