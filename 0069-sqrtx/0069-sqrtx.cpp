1b62964231c22bea2d5af74d5c49380ad2599989
AbdElRahman Osama <bodyusama207@gmail.com>
2024-04-30 23:44:58 +0300

    Time: 0 ms (100%), Space: 7.4 MB (39.24%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.cpp
+++ 0069-sqrtx/0069-sqrtx.cpp
@@ -1,17 +1,13 @@
 class Solution {
 public:
     int mySqrt(int x) {
-        if (x < 2) return x;
-        int st = 1, ed = x, mid;
+        int st = 0, ed = x, mid;
         while (st <= ed) {
-            mid = st + (ed-st)/2;
-            long long sq = static_cast<long long>(mid)*mid;
-            if (sq > x)
-                ed = mid - 1;
-            else if (sq == x)
-                return mid;
-            else 
-                st = mid + 1;
+            mid = st + (ed - st) / 2;
+            long long sq = (long long) mid * mid;
+            if (sq == x) return mid;
+            if (sq > x) ed = mid - 1;
+            else st = mid + 1;
         }
         return ed;
     }
