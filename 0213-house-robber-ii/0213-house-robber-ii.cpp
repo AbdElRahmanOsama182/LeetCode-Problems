d81c505237c5f54ca251c9ce24a32d970e09efc2
AbdElRahman Osama <bodyusama207@gmail.com>
2024-05-01 02:27:15 +0300

    Time: 4 ms (33.08%), Space: 9.3 MB (64.26%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.cpp
+++ 0213-house-robber-ii/0213-house-robber-ii.cpp
@@ -3,14 +3,16 @@ public:
     int rob(vector<int>& nums) {
         int n = nums.size();
         if (n == 1) return nums[0];
-        return max(ropHelper(nums,0,n-1),ropHelper(nums,1,n));
+        int op1 = robber(nums, 0, n - 1);
+        int op2 = robber(nums, 1, n);
+        return max(op1, op2);
     }
-    int ropHelper(vector<int>& nums, int st, int ed) {
-        int prev = 0, prev2 = 0, curr = 0;
+    int robber(vector<int>& nums, int st, int ed) {
+        int op1 = 0, op2 = 0, curr = 0;
         for (int i = st; i < ed; i++) {
-            curr = max(prev,prev2+nums[i]);
-            prev2 = prev;
-            prev = curr;
+            curr = max(op1 + nums[i], op2);
+            op1 = op2;
+            op2 = curr;
         }
         return curr;
     }
