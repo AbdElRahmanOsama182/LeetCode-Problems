40f5dde10bff1f694c6742aab468d135b39da993
AbdElRahman Osama <bodyusama207@gmail.com>
2024-05-01 11:26:23 +0300

    Time: 33 ms (98.03%), Space: 48.3 MB (53.66%) - LeetHub

--- 0208-implement-trie-prefix-tree/0208-implement-trie-prefix-tree.cpp
+++ 0208-implement-trie-prefix-tree/0208-implement-trie-prefix-tree.cpp
@@ -1,13 +1,13 @@
 class TrieNode {
-    public:
+public:
+    bool isWord;
     TrieNode* children[26];
-    bool isEnd;
-    TrieNode(){
-        isEnd = false;
-        for (int i = 0; i < 26; i++)
-            children[i] = nullptr;
+    TrieNode () {
+        isWord = false;
+        for (int i = 0; i < 26; i++) children[i] = nullptr;
     }
 };
+
 
 class Trie {
 public:
@@ -17,32 +17,33 @@ public:
     }
     
     void insert(string word) {
-        TrieNode* current = root;
-        for (char c : word){
-            if (current->children[c - 'a'] == nullptr)
-                current->children[c - 'a'] = new TrieNode();
-            current = current->children[c - 'a'];
+        TrieNode* curr = root;
+        for (auto& c : word) {
+            if (!curr->children[c-'a']) curr->children[c-'a'] = new TrieNode();
+            curr = curr->children[c-'a'];
         }
-        current->isEnd = true;
+        curr->isWord = true;
     }
     
-    bool search(string word) {
-        TrieNode* current = root;
-        for (char c : word){
-            if (current->children[c - 'a'] == nullptr)
-                return false;
-            current = current->children[c - 'a'];
+    bool search(string word, bool prefix = false) {
+        TrieNode* curr = root;
+        for (auto& c : word) {
+            if (!curr->children[c-'a']) return false;
+            curr = curr->children[c-'a'];
         }
-        return current->isEnd;
+        if (!prefix) return curr->isWord;
+        return true;
     }
     
     bool startsWith(string prefix) {
-        TrieNode* current = root;
-        for (char c : prefix){
-            if (current->children[c - 'a'] == nullptr)
-                return false;
-            current = current->children[c - 'a'];
-        }
-        return true;
+        return search(prefix, true);
     }
 };
+
+/**
+ * Your Trie object will be instantiated and called as such:
+ * Trie* obj = new Trie();
+ * obj->insert(word);
+ * bool param_2 = obj->search(word);
+ * bool param_3 = obj->startsWith(prefix);
+ */
