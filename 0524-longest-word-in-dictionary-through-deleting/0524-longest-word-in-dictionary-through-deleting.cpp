21c4a9f23b75c66db730bffc887de6cdcd9c9c01
AbdElRahman Osama <bodyusama207@gmail.com>
2024-05-01 03:56:09 +0300

    Time: 45 ms (82.76%), Space: 17.8 MB (93.24%) - LeetHub

--- 0524-longest-word-in-dictionary-through-deleting/0524-longest-word-in-dictionary-through-deleting.cpp
+++ 0524-longest-word-in-dictionary-through-deleting/0524-longest-word-in-dictionary-through-deleting.cpp
@@ -4,17 +4,14 @@ public:
         string ans = "";
         int n = s.size();
         for (auto& d : dictionary) {
-            int i = 0, j = 0, m = d.size();
-            if (m > n) continue;
+            int i = 0, j = 0;
+            int m = d.size();
             while (i < n && j < m) {
-                if (s[i] == d[j]) {
-                    j++;
-                }
-                i++;
+                if (s[i] == d[j]) i++, j++;
+                else i++;
             }
-            if (j == m && (ans.size() < m || (ans.size() == m && d < ans))) {
+            if (j == m && (m > ans.size() || (m == ans.size() && d < ans)))
                 ans = d;
-            }
         }
         return ans;
     }
