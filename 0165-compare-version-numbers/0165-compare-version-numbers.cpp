a3854ed970179a007d0cb2f989913948bfac91fa
AbdElRahman Osama <bodyusama207@gmail.com>
2024-05-05 21:29:39 +0300

    Time: 3 ms (43.15%), Space: 7.4 MB (50.13%) - LeetHub

--- 0165-compare-version-numbers/0165-compare-version-numbers.cpp
+++ 0165-compare-version-numbers/0165-compare-version-numbers.cpp
@@ -1,19 +1,20 @@
 class Solution {
 public:
-    static int compareVersion(string& v1, string& v2) {
-        const int n1=v1.size(), n2=v2.size();
-        int x1=0, x2=0;
-        for(int i=0, j=0; i<n1 || j<n2; i++, j++){
-            while(i<n1 && v1[i]!='.'){
-                x1=10*x1+(v1[i++]-'0');
+    int compareVersion(string version1, string version2) {
+        int n = version1.size(), m = version2.size();
+        int v1 = 0, v2 = 0;
+        for (int i = 0, j = 0; i < n || j < m; i++, j++) {
+            while (i < n && version1[i] != '.') {
+                v1 = 10 * v1 + (version1[i]-'0');
+                i++;
             }
-            while(j<n2 && v2[j]!='.'){
-                x2=10*x2+(v2[j++]-'0');
+            while (j < m && version2[j] != '.') {
+                v2 = 10 * v2 + (version2[j]-'0');
+                j++;
             }
-            if (x1<x2) return -1;
-            else if (x1>x2) return 1;
-            x1=0;
-            x2=0;
+            if (v1 < v2) return -1;
+            else if (v1 > v2) return 1;
+            else v1 = v2 = 0;
         }
         return 0;
     }
