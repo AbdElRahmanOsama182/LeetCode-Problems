4d8c2c7ad09db87e996e5ac7999b30d2adc39603
AbdElRahman Osama <bodyusama207@gmail.com>
2024-05-15 03:18:26 +0300

    Time: 62 ms (7.07%), Space: 32.2 MB (9.43%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.cpp
+++ 0049-group-anagrams/0049-group-anagrams.cpp
@@ -2,17 +2,17 @@ class Solution {
 public:
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
         unordered_map<string, vector<string>> mp;
-        for(auto s: strs){
+        for(auto& s: strs){
             string hash = "";
             vector<int> freq(26, 0);
-            for(auto c: s) 
+            for(auto& c: s) 
                 freq[c-'a']++;
             for(int i=0; i<26; i++)
                 hash.append(to_string(freq[i]) + "#");
             mp[hash].push_back(s);
         }
         vector<vector<string>> res;
-        for(auto p: mp)
+        for(auto& p: mp)
             res.push_back(p.second);
         return res;
     }
