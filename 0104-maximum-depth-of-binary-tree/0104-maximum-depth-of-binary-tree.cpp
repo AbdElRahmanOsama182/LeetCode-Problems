046bce82da43824e9e3c2d5c1af9ff165561a6be
AbdElRahman Osama <bodyusama207@gmail.com>
2024-05-30 22:42:00 +0300

    Time: 4 ms (78.79%), Space: 17.4 MB (44.13%) - LeetHub

--- 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.cpp
+++ 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.cpp
@@ -12,7 +12,7 @@
 class Solution {
 public:
     int maxDepth(TreeNode* root) {
-        if (root == NULL) return 0;
+        if (!root) return 0;
         return 1 + max(maxDepth(root->left), maxDepth(root->right));
     }
 };
