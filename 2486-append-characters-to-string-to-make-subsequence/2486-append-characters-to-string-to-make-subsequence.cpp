a98c263abd56bb09911833b72dc94050790f3649
AbdElRahman Osama <bodyusama207@gmail.com>
2024-06-03 14:51:50 +0300

    Time: 22 ms (26.94%), Space: 11.8 MB (97.58%) - LeetHub

--- 2486-append-characters-to-string-to-make-subsequence/2486-append-characters-to-string-to-make-subsequence.cpp
+++ 2486-append-characters-to-string-to-make-subsequence/2486-append-characters-to-string-to-make-subsequence.cpp
@@ -3,10 +3,9 @@ public:
     int appendCharacters(string s, string t) {
         int n = s.size(), m = t.size();
         int i = 0, j = 0;
-        while (i < n && j < m){
+        while (i < n && j < m)
             if (s[i++] == t[j])
                 j++;
-        }
         return m - j;
     }
 };
