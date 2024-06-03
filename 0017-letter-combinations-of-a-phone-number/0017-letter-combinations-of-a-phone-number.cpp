a14dedc3697d7b25583035494917ca9da2c9a891
AbdElRahman Osama <bodyusama207@gmail.com>
2024-06-04 01:53:55 +0300

    Time: 4 ms (15.77%), Space: 8.2 MB (63.35%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.cpp
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.cpp
@@ -1,19 +1,22 @@
 class Solution {
 public:
-    vector<string> phone = {"abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
-    void comb(string &digits, int index, string current, vector<string> &combinations){
-        if (index == digits.size()){
+    vector<string> phone = {"abc", "def",  "ghi", "jkl",
+                            "mno", "pqrs", "tuv", "wxyz"};
+    void comb(string& digits, int index, string current,
+              vector<string>& combinations) {
+        if (index == digits.size()) {
             combinations.push_back(current);
             return;
         }
         string letters = phone[digits[index] - '2'];
-        for (char c : letters){
+        for (char c : letters) {
             comb(digits, index + 1, current + c, combinations);
         }
     }
     vector<string> letterCombinations(string digits) {
         vector<string> combinations;
-        if (digits.empty()) return combinations;
+        if (digits.empty())
+            return combinations;
         comb(digits, 0, "", combinations);
         return combinations;
     }
