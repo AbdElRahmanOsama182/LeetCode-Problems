1f073f3303986f4e5df139f30bd54cda6b0e226c
AbdElRahman Osama <bodyusama207@gmail.com>
2024-06-09 02:14:07 +0300

    Time: 0 ms (100%), Space: 7.5 MB (50.41%) - LeetHub

--- 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.cpp
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.cpp
@@ -1,6 +1,15 @@
 class Solution {
 public:
     int strStr(string haystack, string needle) {
-        return haystack.find(needle);
+        if (needle.empty()) return 0;
+        int m = haystack.size(), n = needle.size();
+        for (int i = 0; i <= m - n; i++) {
+            int j = 0;
+            for (; j < n; j++) {
+                if (haystack[i + j] != needle[j]) break;
+            }
+            if (j == n) return i;
+        }
+        return -1;
     }
 };
