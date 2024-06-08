9d759d86616e86ae2121f3cb0f8f22f6b6aa1451
AbdElRahman Osama <bodyusama207@gmail.com>
2024-06-09 02:09:38 +0300

    Time: 0 ms (100%), Space: 7.7 MB (18.82%) - LeetHub

--- 1768-merge-strings-alternately/1768-merge-strings-alternately.cpp
+++ 1768-merge-strings-alternately/1768-merge-strings-alternately.cpp
@@ -1,14 +1,13 @@
 class Solution {
 public:
     string mergeAlternately(string word1, string word2) {
-        int i = 0, j = 0, n = word1.size(), m = word2.size();
+        int i = 0, n = word1.size(), m = word2.size();
         string ans = "";
-        while(i < n && j < m){
-            ans += word1[i++];
-            ans += word2[j++];
+        while(i < n || i < m){
+            if (i < n) ans += word1[i];
+            if (i < m) ans += word2[i];
+            i++;
         }
-        if (i < n) copy(word1.begin() + i, word1.end(), back_inserter(ans));
-        if (j < m) copy(word2.begin() + j, word2.end(), back_inserter(ans));
         return ans;
     }
 };
