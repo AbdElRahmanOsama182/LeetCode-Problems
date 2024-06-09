0e992b6c9763330307c310c548e56ea2bc098ef4
AbdElRahman Osama <bodyusama207@gmail.com>
2024-06-09 11:29:40 +0300

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 1275-find-winner-on-a-tic-tac-toe-game/1275-find-winner-on-a-tic-tac-toe-game.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    string tictactoe(vector<vector<int>>& moves) {
+        vector<vector<int>> board(3, vector<int>(3, 0));
+        for (int i = 0; i < moves.size(); i++) 
+            board[moves[i][0]][moves[i][1]] = i % 2 == 0 ? 1 : -1;
+        int sumR, sumC;
+        for (int i = 0; i < 3; i++) {
+            sumR = sumC = 0;
+            for (int j = 0; j < 3; j++) {
+                sumR += board[i][j];
+                sumC += board[j][i];
+            }
+            if (sumR == 3 || sumC == 3) return "A";
+            if (sumR == -3 || sumC == -3) return "B";
+        }
+        int sumD1 = board[0][0] + board[1][1] + board[2][2];
+        int sumD2 = board[0][2] + board[1][1] + board[2][0];
+        if (sumD1 == 3 || sumD2 == 3) return "A";
+        if (sumD1 == -3 || sumD2 == -3) return "B";
+        return "Draw";
+    }
+};
