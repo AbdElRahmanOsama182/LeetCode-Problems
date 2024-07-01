17b4ea8d7bf07fb5f149c7a117a089a20f115b1c
AbdElRahman Osama <bodyusama207@gmail.com>
2024-07-01 06:06:18 +0300

    Time: 0 ms (100%), Space: 10.5 MB (83.42%) - LeetHub

---
+++ 1550-three-consecutive-odds/1550-three-consecutive-odds.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    bool threeConsecutiveOdds(vector<int>& arr) {
+        int n = arr.size();
+        for (int i = 0; i < n - 2; i++) 
+            if (arr[i] & 1 && arr[i + 1] & 1 && arr[i + 2] & 1)
+                return true;
+        return false;
+    }
+};
