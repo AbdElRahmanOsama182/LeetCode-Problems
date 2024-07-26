040760f9057d66d0a6baf2b9b8b22974dc5da7e4
AbdElRahman Osama <bodyusama207@gmail.com>
2024-07-26 03:12:12 +0300

    Time: 24 ms (74.13%), Space: 15.5 MB (79.53%) - LeetHub

--- 1334-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance/1334-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance.cpp
+++ 1334-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance/1334-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance.cpp
@@ -8,20 +8,16 @@ public:
         }
         for (int i = 0; i < n; i++)
             distance[i][i] = 0;
-        for (int k = 0; k < n; k++) {
-            for (int i = 0; i < n; i++) {
-                for (int j = 0; j < n; j++) {
+        for (int k = 0; k < n; k++) 
+            for (int i = 0; i < n; i++) 
+                for (int j = 0; j < n; j++) 
                     distance[i][j] = min(distance[i][j], distance[i][k] + distance[k][j]);
-                }
-            }
-        }
         int ans = -1, mn = INT_MAX;
         for (int i = 0; i < n; i++) {
             int cnt = 0;
-            for (int j = 0; j < n; j++) {
+            for (int j = 0; j < n; j++)
                 if (distance[i][j] <= distanceThreshold)
                     cnt++;
-            }
             if (cnt <= mn) {
                 mn = cnt;
                 ans = i;
