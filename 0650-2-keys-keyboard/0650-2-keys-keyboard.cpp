1b8bb026a85063040c183614f22483d0d29c743f
AbdElRahman Osama <bodyusama207@gmail.com>
2024-08-19 04:20:04 +0300

    Time: 0 ms (100%), Space: 8.2 MB (30.87%) - LeetHub

--- 0650-2-keys-keyboard/0650-2-keys-keyboard.cpp
+++ 0650-2-keys-keyboard/0650-2-keys-keyboard.cpp
@@ -4,9 +4,8 @@ public:
         vector<int> dp(n+1, INT_MAX);
         dp[1] = 0;
         for (int i = 1; i <= n; i++) 
-            for (int j = 2; j <= n; j++) 
-                if (i*j <= n)
-                    dp[i*j] = min(dp[i*j], dp[i]+j);
+            for (int j = 2; j*i <= n; j++) 
+                dp[i*j] = min(dp[i*j], dp[i]+j);
         return dp[n];
     }
 };
