d1cf56a0352f9d4a47bac70c64304b661145fe83
AbdElRahman Osama <bodyusama207@gmail.com>
2026-02-08 17:16:11 +0200

    Time: 0 ms (100%), Space: 22.6 MB (79.14%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.cpp
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.cpp
@@ -1,7 +1,10 @@
 class Solution {
 public:
-    int removeDuplicates(vector<int>& v) {
-        v.resize(distance(v.begin(),unique(v.begin(), v.end())));
-        return v.size();
+    int removeDuplicates(vector<int>& nums) {
+        int n = nums.size(), idx = 0;
+        for (int i = 1; i < n; i++) {
+            if (nums[i] != nums[idx]) nums[++idx] = nums[i];
+        }
+        return idx+1;
     }
 };
