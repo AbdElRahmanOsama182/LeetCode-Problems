b749985c578fccbafb66a202fcf888b59eac5daf
AbdElRahman Osama <bodyusama207@gmail.com>
2026-02-10 23:39:07 +0200

    Time: 0 ms (100%), Space: 36.1 MB (53.5%) - LeetHub

--- 1926-nearest-exit-from-entrance-in-maze/1926-nearest-exit-from-entrance-in-maze.cpp
+++ 1926-nearest-exit-from-entrance-in-maze/1926-nearest-exit-from-entrance-in-maze.cpp
@@ -1,35 +1,28 @@
 class Solution {
 public:
-    bool isExit(int &x, int &y, int &n, int &m){
-        return x == 0 || x == n - 1 || y == 0 || y == m - 1;
-    }
-
-    bool isValid(int &x, int &y, int &n, int &m){
-        return x >= 0 && x < n && y >= 0 && y < m;
-    }
     int nearestExit(vector<vector<char>>& maze, vector<int>& entrance) {
-        int n = maze.size(), m = maze[0].size(), level = 0;
-        vector<vector<bool>> visited(n, vector<bool>(m, false));
+        vector<int> dir = {1, 0, -1, 0, 1};
         queue<pair<int, int>> q;
+        int n = maze.size(), m = maze[0].size();
+        vector<vector<bool>> vis(n, vector<bool>(m, false));
         q.push({entrance[0], entrance[1]});
-        visited[entrance[0]][entrance[1]] = true;
-        vector<pair<int, int>> dir = {{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
-        while (!q.empty()){
+        vis[entrance[0]][entrance[1]] = true;
+        int lvl = 0;
+        while (q.size()) {
             int sz = q.size();
-            for (int i = 0; i < sz; i++){
-                auto [x, y] = q.front();
+            lvl++;
+            for (int i = 0; i < sz; i++) {
+                auto[x, y] = q.front();
                 q.pop();
-                for (auto [dx, dy] : dir){
-                    int nx = x + dx, ny = y + dy;
-                    if (isValid(nx, ny, n, m) && maze[nx][ny] == '.' && !visited[nx][ny]){
-                        visited[nx][ny] = true;
-                        q.push({nx, ny});
-                        if (isExit(nx, ny, n, m)) return level + 1;
-                    }
-                    
+                for (int d = 0; d < 4; d++) {
+                    int nx = x + dir[d], ny = y + dir[d+1];
+                    if (nx < 0 || nx == n || ny < 0 || ny == m) continue;
+                    if (vis[nx][ny] || maze[nx][ny] == '+') continue;
+                    if (nx == 0 || nx == n-1 || ny == 0|| ny == m-1) return lvl;
+                    vis[nx][ny] = true;
+                    q.push({nx, ny});
                 }
             }
-            level++;
         }
         return -1;
     }
