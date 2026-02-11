ae701f2cf0a66e3a21a2d80ca02a067ade4a8c08
AbdElRahman Osama <bodyusama207@gmail.com>
2026-02-11 06:55:31 +0200

    Time: 0 ms (100%), Space: 17.5 MB (89.18%) - LeetHub

--- 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.cpp
+++ 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.cpp
@@ -1,9 +1,10 @@
 class Solution {
 public:
     int minCostClimbingStairs(vector<int>& cost) {
-        cost.push_back(0);
-        for (int i=2; i<cost.size(); i++) 
-            cost[i] += min(cost[i-1],cost[i-2]);
-        return cost.back();
+        int n = cost.size();
+        for (int i = 2; i < n; i++) {
+            cost[i] += min(cost[i-1], cost[i-2]);
+        }
+        return min(cost[n-1], cost[n-2]);
     }
 };
