bb4639db228f4bc39974113cd795da19e660cbf5
AbdElRahman Osama <bodyusama207@gmail.com>
2026-02-16 20:42:41 +0200

    Time: 6 ms (5.56%), Space: 8.3 MB (15.72%) - LeetHub

--- 0190-reverse-bits/0190-reverse-bits.cpp
+++ 0190-reverse-bits/0190-reverse-bits.cpp
@@ -1,12 +1,13 @@
 class Solution {
 public:
-    uint32_t reverseBits(uint32_t n) {
-        uint32_t res = 0;
-        for(int i=0; i<32; i++){
-            res = res << 1;
-            res = res | (n & 1);
+    int reverseBits(int n) {
+        int ans = 0;
+        for (int i = 0; i < 32; i++) {
+            ans = ans << 1;
+            ans |= (n&1);
             n = n >> 1;
         }
-        return res;
+        std::cout << std::bitset<32>(ans) << '\n';
+        return ans;
     }
 };
